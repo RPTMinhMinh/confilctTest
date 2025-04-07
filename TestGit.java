75fe4c30478a9aa438c2dcfdf275c6779014ad81
Minh <ngnhatminh71203@gmail.com>
2025-04-07 09:00:31 +0700

    stream

--- TestGit.java
+++ TestGit.java
@@ -1,9 +1,16 @@
-
+import java.util.List;
+import java.util.stream.Stream;
 
 public class TestGit {
   public static void main(String[] args) {
     System.out.println("test");
     System.out.println("develop");
     System.out.println("eeee");
+    List<String> list = List.of("a", "b", "c");
+    list.forEach(System.out::println);
+    Stream<String> stream = list.stream();
+//    stream.forEach(System.out::println);
+    stream.filter(i -> i.equals("a")).forEach(System.out::println);
   }
 }
+
